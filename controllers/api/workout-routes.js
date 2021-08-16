e7976daecdc8b2758e93a5b44231b5c98fb7ce2b
qtian13 <qiushuang.tian@gmail.com>
2021-08-16 00:34:42 -0700

    add api route for get workout

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const db = require("../../models");
 
-// /api/workouts
+// fetch /api/workouts
+
+router.get('/', async (req, res) => {
+  try {
+    const workouts = await db.Workout.find({});
+    res.status(200).json(workouts);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+})
 
 module.exports = router;
