50cb1d92bc2501993c50193f8a24e9a01425aa33
qtian13 <qiushuang.tian@gmail.com>
2021-08-16 00:02:36 -0700

    add workout model

---
+++ models/Workout.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Exercise = require('./Exercise');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: new Date()
+  },
+
+  exercises: {
+    type: [ExerciseSchema],
+    default: undefined
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
