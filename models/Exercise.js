09e0227d87d7517ff01f53404d90f16aed9b31be
qtian13 <qiushuang.tian@gmail.com>
2021-08-15 22:39:19 -0700

    add exercide model

---
+++ models/Exercise.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "String is Required"
+  },
+
+  name: {
+    type: String,
+    trim: true,
+    // unique: true,
+    required: "String is Required"
+  },
+
+  weight: {
+    type: Number,
+    min: 0
+  },
+
+  sets: {
+    type: Number,
+    min: 1
+  },
+
+  reps: {
+    type: Number,
+    min: 1
+  },
+
+  duration: {
+    type: Number,
+    min: 0
+  }
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
