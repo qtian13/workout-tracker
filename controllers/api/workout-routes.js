81734753e30c4187e3c78fe26375789e3cc82509
qtian13 <qiushuang.tian@gmail.com>
2021-08-16 18:31:19 -0700

    finish the api get route workouts/range

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -36,6 +36,32 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/range', async (req, res) => {
+  try {
+    // return all workout documents ordered by date created
+    // add temperary field totalDuration
+    // const workouts = Workout.find().sort( { date: 1 } ).limit( 5 )
+    const workouts = await Workout.aggregate([{
+      $addFields:{
+        totalDuration: {
+          $sum: "$exercises.duration"
+        }
+      }
+    }]).sort({ _id: -1 }).limit(7);
+    console.log("====api get all workout in range before reverse=====");
+    console.log(workouts);
+    console.log("====================================================");
+    workouts.reverse();
+    // }]).skip(Workout.count() - 7);
+    console.log("====api get all workout in range after reverse=====");
+    console.log(workouts);
+    console.log("===================================================");
+    res.status(200).json(workouts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 // put /api/workouts/:id     
 // add exercist to existing workout
 router.put('/:id', async (req, res) => {
